b610b6aae05b761bdfdb8cd759f2a51532a7a372
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-07-23 10:08:09 -0300

    TCPServidor Atualizado

---
+++ TCPServidor.WindowsForms/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace TCPServidor.WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEstabelecerConexao = new System.Windows.Forms.Button();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.txtEnderecoIP = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEstabelecerConexao
+            // 
+            this.btnEstabelecerConexao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEstabelecerConexao.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstabelecerConexao.ForeColor = System.Drawing.Color.Chartreuse;
+            this.btnEstabelecerConexao.Location = new System.Drawing.Point(17, 244);
+            this.btnEstabelecerConexao.Name = "btnEstabelecerConexao";
+            this.btnEstabelecerConexao.Size = new System.Drawing.Size(416, 38);
+            this.btnEstabelecerConexao.TabIndex = 28;
+            this.btnEstabelecerConexao.Text = "Estabelecer Conexão";
+            this.btnEstabelecerConexao.UseVisualStyleBackColor = false;
+            this.btnEstabelecerConexao.Click += new System.EventHandler(this.btnEstabelecerConexao_Click);
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Location = new System.Drawing.Point(17, 188);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(227, 20);
+            this.txtPorta.TabIndex = 27;
+            // 
+            // txtEnderecoIP
+            // 
+            this.txtEnderecoIP.Location = new System.Drawing.Point(17, 106);
+            this.txtEnderecoIP.Name = "txtEnderecoIP";
+            this.txtEnderecoIP.Size = new System.Drawing.Size(227, 20);
+            this.txtEnderecoIP.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label7.Location = new System.Drawing.Point(14, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 18);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Porta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(14, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 18);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Endereço IP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 29);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Recebendo Arquivos - Servidor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(322, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(457, 312);
+            this.Controls.Add(this.btnEstabelecerConexao);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.txtEnderecoIP);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEstabelecerConexao;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.TextBox txtEnderecoIP;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TCPServidor.WindowsForms/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPServidor.WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public delegate void FileRecievedEventHandler(object fonte, string nomeArquivo);
+        public event FileRecievedEventHandler NovoArquivoRecebido;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.NovoArquivoRecebido += new FileRecievedEventHandler(Form1_NovoArquivoRecebido);
+        }
+
+        private void Form1_NovoArquivoRecebido(object fonte, string nomeArquivo)
+        {
+            this.BeginInvoke(
+             new Action(
+             delegate ()
+             {
+                 MessageBox.Show("Novo Arquivo recebido\n" + nomeArquivo);
+                 System.Diagnostics.Process.Start("explorer", @"c:\");
+             }));
+        }
+
+        private void btnEstabelecerConexao_Click(object sender, EventArgs e)
+        {
+            int porta = int.Parse(txtPorta.Text);
+            string endIP = txtEnderecoIP.Text;
+            try
+            {
+                Task.Factory.StartNew(() => TratamentoArquivoRecebido(porta, endIP));
+                MessageBox.Show("Escutando na porta...: " + porta);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+
+        }
+
+        public void TratamentoArquivoRecebido(int porta, string enderecoIp)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse(enderecoIp);
+                TcpListener tcpListener = new TcpListener(ip, porta);
+                tcpListener.Start();
+
+                while (true)
+                {
+                    Socket manipularSocket = tcpListener.AcceptSocket();
+                    if (manipularSocket.Connected)
+                    {
+                        string nomeArquivo = string.Empty;
+                        NetworkStream networkStream = new NetworkStream(manipularSocket);
+                        int thisRead = 0;
+                        int blockSize = 1024;
+                        Byte[] dataByte = new Byte[blockSize];
+                        lock (this)
+                        {
+                            string caminhoPastaDestino = @"c:\dados";
+                            manipularSocket.Receive(dataByte);
+                            int tamanhoNomeArquivo = BitConverter.ToInt32(dataByte, 0);
+                            nomeArquivo = Encoding.ASCII.GetString(dataByte, 4, tamanhoNomeArquivo);
+                            //
+                            Stream fileStream = File.OpenWrite(caminhoPastaDestino + nomeArquivo);
+                            fileStream.Write(dataByte, 4 + tamanhoNomeArquivo, (1024 - (4 + tamanhoNomeArquivo)));
+                            while (true)
+                            {
+                                thisRead = networkStream.Read(dataByte, 0, blockSize);
+                                fileStream.Write(dataByte, 0, thisRead);
+                                if (thisRead == 0)
+                                    break;
+                            }
+                            //
+                            fileStream.Close();
+                        }
+                        NovoArquivoRecebido?.Invoke(this, nomeArquivo);
+                        manipularSocket = null;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+       
+    }
+}
